ffa16796effd5bd7eb6415d602b914f72d9a7b55
DiogoPombo <150866276+DiogoPombo@users.noreply.github.com>
2024-08-01 14:57:13 -0300

    Add files via upload

---
+++ IE Chooser.bat
@@ -0,0 +1,10 @@
+@echo off
+set APP=IE Chooser
+set AUTHOR=POMBO
+set AVATAR=\Õ/
+set MADE_BY=MADE BY:
+set SPACE= 
+set KEY=EWEP
+echo %APP%%SPACE%%MADE_BY%%SPACE%%SPACE%%AUTHOR%%SPACE%%SPACE%%AVATAR%%SPACE%%KEY%
+start %systemroot%\system32\f12\IEChooser.exe
+exit
