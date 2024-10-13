f00cec0b110c110bcf99b2b2ad4810a0cd538a12
DiogoPombo <150866276+DiogoPombo@users.noreply.github.com>
2024-10-13 12:56:31 -0300

    Final version!
    Added open program confirmation, error checking and error message to user.

--- IE Chooser.bat
+++ IE Chooser.bat
@@ -1,10 +1,32 @@
 @echo off
+@title IE Chooser
+
 set APP=IE Chooser
 set AUTHOR=POMBO
 set AVATAR=\Õ/
 set MADE_BY=MADE BY:
 set SPACE= 
-set KEY=EWEP
+set KEY=@EWEP - 2024
+
+COLOR 0F
+
 echo %APP%%SPACE%%MADE_BY%%SPACE%%SPACE%%AUTHOR%%SPACE%%SPACE%%AVATAR%%SPACE%%KEY%
+
+echo.
+echo Please wait, starting IE Chooser. . .
+echo.
+
 start %systemroot%\system32\f12\IEChooser.exe
+if errorlevel 1 (
+    COLOR 0C
+    echo Error: Could not start IE Chooser at "%systemroot%\system32\f12\IEChooser.exe". Please check if the file exists and the user has sufficient privileges to access it and try again.
+    echo.
+    echo Press any key to exit!
+    pause >nul
+    exit
+)
+
+COLOR 0B
+echo Done!
+
 exit
