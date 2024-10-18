dc643b321bdcb3c2fe386dfcc3c106306c45af27
DiogoPombo <150866276+DiogoPombo@users.noreply.github.com>
2024-10-18 08:53:46 -0300

    Main!

--- IE Chooser.bat
+++ IE Chooser.bat
@@ -1,6 +1,12 @@
 @echo off
 @title IE Chooser
 COLOR F0
+cls
+echo.
+echo.
+echo   ************************* IE Chooser *************************
+echo.
+echo.
 
 setlocal
 set APP=IE Chooser
@@ -12,30 +18,35 @@ set KEY=@EWEP - 2024
 echo %APP%%SPACE%%MADE_BY%%SPACE%%SPACE%%AUTHOR%%SPACE%%SPACE%%AVATAR%%SPACE%%KEY%
 
 echo.
-echo: ************************* IE Chooser *************************
-echo.
-echo IE Chooser is a debugger (DevTools - F12) focused on Internet Explorer and Microsoft Edge's IE mode!
+echo   IE Chooser is a debugger (DevTools - F12) focused on Internet Explorer and Microsoft Edge's IE mode!
 
 echo.
-echo Please wait, attempting to start IE Chooser. . .
+echo   Please wait, attempting to start IE Chooser. . .
 start %systemroot%\system32\f12\IEChooser.exe
 if errorlevel 1 (
     cls
     COLOR F4
     echo.
-    echo ERROR: Could not start IE Chooser at "%systemroot%\system32\f12\IEChooser.exe"!
+    echo   ERROR: Could not start IE Chooser at "%systemroot%\system32\f12\IEChooser.exe"!
     echo.
-    echo Please check if the file exists in this path and it's not corrupted.
-    echo Please check if the user has sufficient privileges to access it.
+    echo   Please check if the file exists in this path and it's not corrupted.
+    echo   Please check if the user has sufficient privileges to access it.
     echo.
-    echo  Press any key to exit!
+    echo   Press any key to exit!
     endlocal
     pause >nul
     exit
 )
 
+cls
 echo.
-echo Welcome to Internet Explorer DevTools!
+echo.
+echo   ************************* IE Chooser *************************
+echo.
+echo.
+
+echo   Welcome to Internet Explorer DevTools!
+timeout /t 3 /nobreak >nul
 
 endlocal
 exit
